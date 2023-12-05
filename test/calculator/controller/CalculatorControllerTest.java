5153f499a9eb1610a433eef92675510a424d13c5
UTENTE <UTENTE@172.19.229.26>
2023-12-05 11:15:31 +0100

    Initial version all files

--- test/calculator/controller/CalculatorControllerTest.java
+++ test/calculator/controller/CalculatorControllerTest.java
@@ -39,7 +39,8 @@ public class CalculatorControllerTest {
     @Test
     public void testSomeMethod() {
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
+        System.out.println("cazzo cazzissimo");
     }
     
 }
